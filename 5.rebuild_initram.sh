35f8e3b93d4bcfe12e5e9c5da92988389d6b5d50
styper <erick.vecchiato@gmail.com>
2018-08-06 19:09:16 -0300

    Update 5.rebuild_initram.sh

--- 5.rebuild_initram.sh
+++ 5.rebuild_initram.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-sudo mkinitramfs -o /boot/initramfs.gz
+# Make sure only root can run our script
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+mkinitramfs -o /boot/initramfs.gz
 lsinitramfs /boot/initramfs.gz |grep -P "sbin/(cryptsetup|resize2fs|fdisk|dumpe2fs|expect)"
 #sudo reboot
